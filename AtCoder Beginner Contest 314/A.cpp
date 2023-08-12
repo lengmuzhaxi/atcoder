312f7d8f6927ea96225f3653f9f75137296c9c73
lengmuzashi <2690497440@qq.com>
2023-08-12 23:54:15 +0800

    AtCoder Beginner Contest 314

---
+++ AtCoder Beginner Contest 314/A.cpp
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	string s="3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679";
+//	int n = 0;
+//	cin >> n;
+//	cout << s.substr(0, n+2);
+//}
