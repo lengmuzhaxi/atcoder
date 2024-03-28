d62383647b0326898a032504e573c86aeff760f7
lengmuzhaxi <2690497440@qq.com>
2024-03-28 22:09:40 +0800

    AtCoder Beginner Contest 346

---
+++ AtCoder Beginner Contest 346/C.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include<set>
+using namespace std;
+using ll = long long;
+int main() 
+{
+    int n, k;
+    cin >> n >> k;
+    set<int> st;
+    for (int i = 0; i < n; i++) 
+    {
+        int a;
+        cin >> a;
+        if (a <= k)
+        {
+            st.insert(a);
+        }
+    }
+    ll ans = (ll)k * (k + 1) / 2;
+    for (int i : st)
+    {
+        ans -= i;
+    }
+    cout << ans << endl;
+}
