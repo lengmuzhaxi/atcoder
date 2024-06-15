8679e320d90526aa634629875820d7538dffdad0
lengmuzhaxi <2690497440@qq.com>
2024-06-15 22:20:09 +0800

    AtCoder Beginner Contest 358

---
+++ AtCoder Beginner Contest 358/A.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string a,b;
+    cin>>a>>b;
+    if(a=="AtCoder"&&b=="Land")
+    {
+        cout<<"Yes";
+    }
+    else
+    {
+        cout<<"No";
+    }
+}//
+// Created by dzl on 24-6-15.
+//
