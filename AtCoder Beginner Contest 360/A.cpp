8e6ea8615bf247682e081311b37f962b9210ad72
lengmuzhaxi <2690497440@qq.com>
2024-06-30 23:59:26 +0800

    AtCoder Beginner Contest 360

---
+++ AtCoder Beginner Contest 360/A.cpp
@@ -0,0 +1,16 @@
+/*#include<bits/stdc++.h>
+#include<string>
+using namespace std;
+int main()
+{
+    string s;
+    cin>>s;
+    if(s=="RSM"||s=="RMS")
+    {
+        cout<<"Yes";
+    }
+    else
+    {
+        cout<<"No";
+    }
+}*/
