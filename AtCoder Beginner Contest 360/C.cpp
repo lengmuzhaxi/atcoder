465d54031bcaa690319b79e1f4189da9ba8cb359
lengmuzhaxi <2690497440@qq.com>
2024-07-06 23:42:58 +0800

    AtCoder Beginner Contest 360

---
+++ AtCoder Beginner Contest 360/C.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string a,b;
+    string now;
+    cin >> a >> b;
+    for(int w=1;w<a.size();++w)
+    {
+        for(int c=0;c<w;++c)
+        {
+            now ="";
+            for(int i=c;i<a.size();i+=w )
+            {
+                now+=a[i];
+            }
+            if(now == b)
+            {
+                cout << "Yes" << endl;
+                return 0;
+            }
+        }
+    }
+    cout << "No" << endl;
+    return 0;
+}
