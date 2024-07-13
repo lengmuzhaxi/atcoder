c701fc4acc3acf4a39a742b9ce37d88d55050502
lengmuzhaxi <2690497440@qq.com>
2024-07-13 23:45:14 +0800

    AtCoder Beginner Contest 362

---
+++ AtCoder Beginner Contest 362/A.cpp
@@ -0,0 +1,20 @@
+/*#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    string s;
+    cin>>a>>b>>c>>s;
+    if(s=="Red")
+    {
+        cout<<min(b,c);
+    }
+    else if(s=="Green")
+    {
+        cout<<min(a,c);
+    }
+    else
+    {
+        cout<<min(a,b);
+    }
+}*/
