54134ee31daca5e9c57bb18cf81520d45dacd6e8
lengmuzhaxi <2690497440@qq.com>
2024-07-15 22:31:16 +0800

    AtCoder Beginner Contest 362

---
+++ AtCoder Beginner Contest 362/B.cpp
@@ -0,0 +1,23 @@
+/*#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<int>v;
+    int a,b,c,d,e,f;
+    cin>>a>>b>>c>>d>>e>>f;
+    int k=(a-c)*(a-c)+(b-d)*(b-d);
+    int j =(a-e)*(a-e)+(b-f)*(b-f);
+    int n=(d-f)*(d-f)+(c-e)*(c-e);
+    v.push_back(k);
+    v.push_back(n);
+    v.push_back(j);
+    sort(v.begin(),v.end());
+    if(v[0]+v[1]==v[2])
+    {
+        cout<<"Yes";
+    }
+    else
+    {
+        cout<<"No";
+    }
+}*/
