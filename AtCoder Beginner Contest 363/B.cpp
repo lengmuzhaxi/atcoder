6972cf5050d74d6b2878e8f3468f452af0df0343
lengmuzhaxi <2690497440@qq.com>
2024-07-22 00:03:09 +0800

    AtCoder Beginner Contest 363

---
+++ AtCoder Beginner Contest 363/B.cpp
@@ -0,0 +1,20 @@
+/*#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,t,p,a[1000];
+    cin>>n>>t>>p;
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    sort(a,a+n);
+    if(a[n-p]>=t)
+    {
+        cout<<0;
+    }
+    else
+    {
+        cout<<t-a[n-p];
+    }
+}*/
