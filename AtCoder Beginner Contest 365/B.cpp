39a584475d3b73eb777c9fced9f70c65d0815c85
lengmuzhaxi <2690497440@qq.com>
2024-08-04 22:30:28 +0800

    AtCoder Beginner Contest 365

---
+++ AtCoder Beginner Contest 365/B.cpp
@@ -0,0 +1,22 @@
+/*#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int>a(n),b;
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+        b.push_back(a[i]);
+    }
+    sort(b.begin(),b.end());
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]==b[n-2])
+        {
+            cout<<i+1;
+            break;
+        }
+    }
+}*/
