05cb221ae5cf2a7783364525ea021a282ff86222
lengmuzhaxi <2690497440@qq.com>
2024-09-08 10:19:13 +0800

    AtCoder Beginner Contest 370

---
+++ AtCoder Beginner Contest 370/A.cpp
@@ -0,0 +1,19 @@
+/*#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    if(a+b==1&&a==1)
+    {
+        cout<<"Yes";
+    }
+    else if(a+b==1&&b==1)
+    {
+        cout<<"No";
+    }
+    else
+    {
+        cout<<"Invalid";
+    }
+}*/
