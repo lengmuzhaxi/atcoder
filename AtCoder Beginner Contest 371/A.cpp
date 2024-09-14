cc71635a2ef1aafde56b3bb826a7d420a44c6fd6
党子立 <2690497440@qq.com>
2024-09-14 22:18:10 +0800

    AtCoder Beginner Contest 371

---
+++ AtCoder Beginner Contest 371/A.cpp
@@ -0,0 +1,26 @@
+/*
+#include <iostream>
+using namespace std;
+
+int main() {
+    char S_AB, S_AC, S_BC;
+    // Take input from the user
+    cin >> S_AB >> S_AC >> S_BC;
+    // Check all possible combinations
+    if (S_AB == '<' && S_AC == '<' && S_BC == '<') {
+        cout << "B" << endl;  // A < B < C
+    } else if (S_AB == '<' && S_AC == '<'&& S_BC == '>') {
+        cout << "A" << endl;  // B < A < C
+    } else if (S_AB == '<' && S_AC == '>' && S_BC == '>') {
+        cout << "A" << endl;  // A < C < B
+    } else if (S_AB == '>' && S_AC == '>' && S_BC == '>') {
+        cout << "B" << endl;  // C < B < A
+    } else if (S_AB == '>' && S_AC == '>' && S_BC == '<') {
+        cout << "B" << endl;  // A < C < B
+    } else if (S_AB == '>' && S_AC == '<' && S_BC == '<') {
+        cout << "A" << endl;  // B < A < C
+    }
+
+    return 0;
+}
+*/
