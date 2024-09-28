900c2f2dadf4df8e5778b5d7d8ba866e6ce9faeb
lengmuzhaxi <2690497440@qq.com>
2024-09-28 21:31:59 +0800

    AtCoder Beginner Contest 373

---
+++ AtCoder Beginner Contest 373/A.cpp
@@ -0,0 +1,13 @@
+/*#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int cnt=0;
+    vector<string>s(12);
+    for(int i=0;i<12;i++) {
+        cin>>s[i];
+        if(s[i].length()==i+1) {
+            cnt++;
+        }
+    }
+    cout<<cnt;
+}*/
