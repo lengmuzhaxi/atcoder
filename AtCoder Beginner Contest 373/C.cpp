f322a437ab5698d14b20f09f2c56db262be223ec
lengmuzhaxi <2690497440@qq.com>
2024-09-30 23:27:50 +0800

    AtCoder Beginner Contest 373

---
+++ AtCoder Beginner Contest 373/C.cpp
@@ -0,0 +1,18 @@
+/*#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    int a[n],b[n];
+    for(int i=0;i<n;i++) {
+        cin>>a[i];
+    }
+    for(int i=0;i<n;i++) {
+        cin>>b[i];
+    }
+    int max_A = *max_element(a, a+n);
+    int max_B = *max_element(b,b+n);
+
+    // Output the sum of the maximum elements
+    cout << max_A + max_B << endl;
+}*/
