099c1123b6e230445acf1ffb29a4ffc30dd23a1a
lengmuzhaxi <2690497440@qq.com>
2024-10-05 22:35:10 +0800

    AtCoder Beginner Contest 374

---
+++ AtCoder Beginner Contest 374/A.cpp
@@ -0,0 +1,13 @@
+/*#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    string s;
+    cin>>s;
+    int n=s.size();
+    if(s.substr(n-3,n)=="san") {
+        cout<<"Yes";
+    }
+    else {
+        cout<<"No";
+    }
+}*/
