e5e05a1cbc69ad9b50fd31a01dd92333da3e79a9
lengmuzhaxi <2690497440@qq.com>
2024-10-06 23:38:40 +0800

    AtCoder Beginner Contest 374

---
+++ AtCoder Beginner Contest 374/B.cpp
@@ -0,0 +1,21 @@
+/*
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s,t;
+    cin>>s>>t;
+    for(int i=0;i<min(s.size(),t.size());i++)
+    {
+        if(s[i]!=t[i])
+        {
+            cout<<i+1;
+            return 0;
+        }
+    }
+    if(s.size()==t.size())
+        cout<<'0'<<endl;
+    else
+        cout<<min(s.size(),t.size())+1<<endl;
+}
+*/
