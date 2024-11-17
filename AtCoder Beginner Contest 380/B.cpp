a4e29a73d42f459d60496d26cf350bbfacf44e5d
lengmuzhaxi <2690497440@qq.com>
2024-11-17 21:55:12 +0800

    AtCoder Beginner Contest 380

---
+++ AtCoder Beginner Contest 380/B.cpp
@@ -0,0 +1,20 @@
+/*#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin>>s;
+    vector<int>a;
+    int j=0,cnt=0;
+    for(int i=0;i<s.size();i++)
+    {
+        if(s[i]!='-')
+        {
+           a.push_back(i);
+        }
+    }
+    for(int i=0;i<a.size()-1;i++)
+    {
+        cout<<a[i+1]-a[i]-1<<' ';
+    }
+}*/
