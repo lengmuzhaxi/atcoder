13150058c44670e7de2063a0f6a360c43b9ddcef
lengmuzhaxi <2690497440@qq.com>
2024-11-23 22:19:23 +0800

    AtCoder Beginner Contest 381

---
+++ AtCoder Beginner Contest 381/A.cpp
@@ -0,0 +1,11 @@
+/*#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N;
+    string S;
+    cin >> N >> S;
+    int n = N / 2;
+    string T = string(n, '1') + '/' + string(n, '2');
+    cout << (S == T ? "Yes" : "No") << endl;
+}*/
