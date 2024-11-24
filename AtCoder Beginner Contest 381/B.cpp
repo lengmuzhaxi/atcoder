bed9f1f77c643dab80948f66bfd462a2032ca4e6
lengmuzhaxi <2690497440@qq.com>
2024-11-24 22:09:03 +0800

    AtCoder Beginner Contest 381

---
+++ AtCoder Beginner Contest 381/B.cpp
@@ -0,0 +1,17 @@
+/*#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s;
+    cin>>s;
+    for(char i='a';i<='z';i++){
+        if(s.find(i)!=string::npos){
+            int t=s.find(i);
+            if(s.find_last_of(i)!=t+1){
+                cout<<"No"<<endl;
+                return 0;
+            }
+        }
+    }
+    cout<<"Yes"<<endl;
+    return 0;
+}*/
