fe21cc8fd40f7aacb08e346da24d14aba925f361
lengmuzhaxi <2690497440@qq.com>
2024-11-30 22:02:53 +0800

    AtCoder Beginner Contest 382

---
+++ AtCoder Beginner Contest 382/A.cpp
@@ -0,0 +1,16 @@
+/*#include<iostream>
+using namespace std;
+int main()
+{
+    int n,k=0,cnt=0;
+    string s;
+    cin>>n>>k>>s;
+    for (int i=0;i<n;i++)
+    {
+        if (s[i]=='@')
+        {
+            cnt++;
+        }
+    }
+    cout<<n-cnt+k;
+}*/
