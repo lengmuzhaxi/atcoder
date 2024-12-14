3599555ba7de5711f6d83071cad16ca74f5ed527
lengmuzhaxi <2690497440@qq.com>
2024-12-14 23:59:12 +0800

    AtCoder Beginner Contest 384

---
+++ AtCoder Beginner Contest 384/A.cpp
@@ -0,0 +1,16 @@
+/*#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n;
+    string s;
+    char c1,c2;
+    cin >> n >> c1 >> c2 >> s;
+
+    for(auto c : s)
+        if(c == c1) cout << c1;
+        else cout << c2;
+
+    return 0;
+}*/
