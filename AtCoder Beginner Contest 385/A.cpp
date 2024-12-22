a3b0a4cd258a71083e9b9b8af8ff1b92d0530fac
lengmuzhaxi <2690497440@qq.com>
2024-12-22 23:55:46 +0800

    AtCoder Beginner Contest 385

---
+++ AtCoder Beginner Contest 385/A.cpp
@@ -0,0 +1,21 @@
+/*
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a[3]={0};
+    for(int i=0;i<3;i++)
+    {
+        cin>>a[i];
+    }
+    sort(a,a+3);
+    if (a[0]+a[1]==a[2]||(a[1]==a[2])&&(a[2]==a[0]))
+    {
+        cout<<"Yes"<<endl;
+    }
+    else
+    {
+        cout<<"No"<<endl;
+    }
+}
+*/
